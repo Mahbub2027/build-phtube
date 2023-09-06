b152f4243c61e3b97f6a576555f8f4dc738eb74d
Mahbub2027 <alam201127@gmail.com>
2023-09-07 01:23:01 +0600

    final updated

--- js/script.js
+++ js/script.js
@@ -6,11 +6,8 @@ const handleCategory = async() => {
     const tabContainer = document.getElementById('tab-container');
     data.data.forEach(categores => {
 
-        
-
         const div = document.createElement('div');
 
-        
         div.innerHTML= `
             <a onclick="handleloadNews('${categores.category_id}')" class="btn mx-2 w-24">${categores.category}</a>
         `;
@@ -38,10 +35,12 @@ const handleloadNews = async(categoryId) => {
 
         data.data?.forEach((news) => {
         const card = document.createElement('div');
+
         card.innerHTML= `
         <div class="card w-72 h-96 bg-base-100 shadow-xl mt-10">
         <figure><img class="w-72 h-40 relative" src="${news?.thumbnail}" alt="Shoes" /></figure>
-        <p class="flex justify-end">${news.others.posted_date}</p>
+        <p class="flex justify-end "></p>
+
         <div class="p-5 ">
         <h2 class="card-title">${news.title}</h2>
         <p class=""></p>
@@ -62,12 +61,162 @@ const handleloadNews = async(categoryId) => {
        `;
        cardContainer.appendChild(card);
     })
+      
+      
 };
-
+   
 
 handleCategory();
 handleloadNews(1000);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* <p>${news.others.posted_date}</p> */
+  
+
+        //const inputSeconds = ${news.others.posted_date}; // Example input: 3665 seconds
+
+    // Step 2: Calculate hours, minutes, and seconds
+    //const hours = Math.floor(inputSeconds / 3600);
+    //const minutes = Math.floor((inputSeconds % 3600) / 60);
+    //const seconds = inputSeconds % 60;
+
+    // Step 3: Format the result as a string
+    //const formattedTime = `${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+
+    // Output the result
+    //console.log(formattedTime);
+
+
+
+   /*  const handleload = (categoryId) => {
+      fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
+      .then((res) => res.json())
+      .then((data) =>{
+        const timeInSeconds = data.data?.others?.posted_date;
+
+        const hours = Math.floor(timeInSeconds / 3600);
+        const minutes = Math.floor((timeInSeconds % 3600) / 60);
+        const seconds = timeInSeconds % 60;
+
+        const formattedTime = `${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+        } 
+      )
+ */
+      /* function formatTime(seconds) {
+        const hours = Math.floor(seconds / 3600);
+        const minutes = Math.floor((seconds % 3600) / 60);
+        const sec = seconds % 60;
+        return `${hours} hours ${minutes} min${sec} sec`;
+      }
+        const inputSeconds = `${data.data.others?.posted_date}`; 
+        const formattedTime = formatTime(inputSeconds);
+        //console.log(formattedTime);
+    } */
+  
+    /* function formatTime(seconds) {
+      const hours = Math.floor(seconds / 3600);
+      const minutes = Math.floor((seconds % 3600) / 60);
+      const sec = seconds % 60;
+      return `${hours} hours ${minutes} min${sec} sec`;
+    }
+      const inputSeconds = `${data.data.others?.posted_date}`; 
+      const formattedTime = formatTime(inputSeconds);
+      console.log(formattedTime); */
+
+
+   /*  // Step 1: Make an HTTP GET request to the API
+      const apiUrl = 'https://openapi.programming-hero.com/api/videos/category/'; // Replace with your API URL
+      fetch(apiUrl)
+      .then((response) => {
+      if (!response.ok) {
+      throw new Error('Network response was not ok');
+        }
+      return response.json();
+        })
+      .then((data) => {
+        const timeInSeconds = data.data?.others?.posted_date;
+
+        const hours = Math.floor(timeInSeconds / 3600);
+        const minutes = Math.floor((timeInSeconds % 3600) / 60);
+        const seconds = timeInSeconds % 60;
+
+        const formattedTime = `${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+        //console.log(formattedTime);
+  }) */
+        /* .catch((error) => {
+         console.error('Error:', error);
+  }
+  ); */
+
+
+
+
+    // Math.floor(news.others.posted_date / 3600 )
+
+
+
+ /* // Step 1: Get the input number (time in seconds)
+    const inputSeconds = news.others.posted_date; // Example input: 3665 seconds
+
+    // Step 2: Calculate hours, minutes, and seconds
+    const hours = Math.floor(inputSeconds / 3600);
+    const minutes = Math.floor((inputSeconds % 3600) / 60);
+    const seconds = inputSeconds % 60;
+
+    // Step 3: Format the result as a string
+    const formattedTime = `${hours} hours, ${minutes} minutes, ${seconds} seconds`;
+
+    // Output the result
+    console.log(formattedTime); // Output: 1 hours, 1 minutes, 5 seconds
+ */
+
+  ///For time
+      /* const seconds = news.data.posted_date;
+       function convertSecondsToHoursMinutesSeconds(seconds) {
+           const hours = Math.floor(seconds / 3600);
+           const remainingSeconds = seconds % 3600;
+           const minutes = Math.floor(remainingSeconds / 60);
+           const remainingSecondsAfterMinutes = remainingSeconds % 60;
+         
+           return {
+             hours,
+             minutes,
+             seconds: remainingSecondsAfterMinutes,
+           };
+         }
+         //const totalSeconds = news.data.posted_date; 
+         const timeObject = convertSecondsToHoursMinutesSeconds(totalSeconds);
+         
+         console.log(`${timeObject.hours} hours, ${timeObject.minutes} minutes, ${timeObject.seconds} seconds`);
+ */
+
+
+    //function sortByView(){
+/*     const sortdatahandle = async(categoryId) =>{
+      const sortData = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+      const sortView = await sortData.json();
+      //console.log(sortView.data);
+
+      const sortDataView = document.getElementById('sort-by-view');
+      sortView.others?.views.sort((a,b) => {
+          return a.others.views - b.others.views;
+        });
+        sortView.others?.views.forEach((e) =>{
+          console.log(`${sortView.others.views}`);
+        })
+    } */
+//}
